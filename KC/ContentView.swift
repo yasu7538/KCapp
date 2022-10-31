0cff05186617decdec5250024d238eb8feea2e0a
Yasu7538 <s2012142@st.tohtech.ac.jp>
2022-10-31 17:32:41 +0900

    Initial Commit

---
+++ KC/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  KC
+//
+//  Created by miuraken on 2022/10/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KC/KCApp.swift
@@ -0,0 +1,17 @@
+//
+//  KCApp.swift
+//  KC
+//
+//  Created by miuraken on 2022/10/31.
+//
+
+import SwiftUI
+
+@main
+struct KCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
